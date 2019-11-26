2fa2dc3d659fba6da237a6f506d9460c2da2c9c9
Mauville <4991127@outlook.com>
2019-11-26 00:23:14 -0600

    Implement Button Listeners

--- src/main/java/observertree/Controller.java
+++ src/main/java/observertree/Controller.java
@@ -1,22 +1,39 @@
 package observertree;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import observabletree.AVLTree;
 
 import java.util.Observable;
 import java.util.Observer;
 
 public class Controller implements Observer {
-    @Override
+    @FXML
+    TextField inputbox;
 
+    // Method gets called on every initialization of the FXMl
+//    @FXML
+//    public initialize(){
+//        AVLTree.setObserver(this);
+//    }
+
+    @Override
     public void update(Observable o, Object arg) {
         clear();
-       render();
+        render();
     }
 
     @FXML
     public void clear() {
     }
 
+    @FXML
+    public void handleAdd() {
+//       AVLTree.add(inputbox.getText());
+    }
+
     private void render() {
     }
+
 }
